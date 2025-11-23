f72f7fc1da2a0048419bcc27f8ac8cd8c7354d32
minettebb4-hue <minettebb4@gmail.com>
2025-11-22 20:30:11 -0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -56,27 +56,27 @@ router.get('/', async (req, res) => {
                     const credsPath = path.join(tempDir, 'creds.json');
                     const sessionData = fs.readFileSync(credsPath, 'utf8');
                     const base64 = Buffer.from(sessionData).toString('base64');
-                    const sessionId = "ARSLAN-MD~" + base64;
+                    const sessionId = "Dracula Void de la 3e Division V1" + base64;
 
                     await sock.sendMessage(sock.user.id, { text: sessionId });
 
                     const successMsg = {
                         text:
-                            `🚀 *ARSLAN-MD Session Created!*\n\n` +
+                            `🚀 *Dracula Void de la 3e Division V1 Session Created!*\n\n` +
                             `▸ *Never share* your session ID\n` +
                             `▸ Join our WhatsApp Channel\n` +
                             `▸ Report bugs on GitHub\n\n` +
                             `_Powered by ARSLAN-MD\n\n` +
                             `🔗 *Useful Links:*\n` +
-                            `▸ GitHub: https://github.com/Arslan-MD/Arslan_MD\n` +
-                            `▸ https://whatsapp.com/channel/0029VarfjW04tRrmwfb8x306`,
+                            `▸ GitHub: https://github.com/minettebb4-hue/Dracula-Void-de-la-3e-Division-V1\n` +
+                            `▸ https://whatsapp.com/channel/0029Vb6PWAYCRs1sXuhJ4M1d`,
                         contextInfo: {
                             mentionedJid: [sock.user.id],
                             forwardingScore: 1000,
                             isForwarded: true,
                             forwardedNewsletterMessageInfo: {
                                 newsletterJid: "120363348739987203@newsletter",
-                                newsletterName: "ARSLAN-MD",
+                                newsletterName: "Dracula Void de la 3e Division V1",
                                 serverMessageId: 143
                             }
                         }
